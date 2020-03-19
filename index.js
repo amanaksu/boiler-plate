ac04843ead8adbfa489309851d25fb59aa4847e2
amanaksu <amanaksu@gmail.com>
2020-03-20 03:00:13 +0900

    Coding the Route part 1 - Route User Register

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// Get User Schema 
+const { User } = require("./models/User");
+
+// Get Body-Parser & Set Options
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));   // application/x-www-form-urlencoded 데이터를 가져올 수 있도록 설정
+app.use(bodyParser.json());                         // application/json 데이터를 가져올 수 있도록 설정
+
 // Set MongoDB 
-const password = "3584ksu";
+const password = "1234qwer";
 const mongoose = require("mongoose");
 mongoose.connect(`mongodb+srv://Kei:${password}@boiler-plate-sdh1y.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
@@ -17,5 +25,29 @@ mongoose.connect(`mongodb+srv://Kei:${password}@boiler-plate-sdh1y.mongodb.net/t
 }).then(() => console.log("Connected MongoDB!")).catch(err => console.log(err));
 
 // Front-End : App 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+// Route "회원가입"
+app.post("/register", (req, res) => {
+    // 회원 가입시 필요한 정보를 Client에서 가져오면
+    // 정보를 DB에 넣어준다. 
+
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) {                  // 저장 실패
+            return res.json({
+                success: false,
+                err
+            });
+        } else {                    // 저장 성공
+            return res.status(200).json({
+                success: true
+            });
+        }
+    });
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
