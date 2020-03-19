066eb8400ca2343d212e1541adfe3c93d0ab6ae7
amanaksu <amanaksu@gmail.com>
2020-03-20 04:10:14 +0900

    Coding the DB part 2 - Encrypted Password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+
 const userSchema = mongoose.Schema({
     firstname: {
         type: String,
@@ -27,6 +31,30 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {         // token 유효기간
         type: Number
+    }
+});
+
+userSchema.pre("save", function( next ) {
+    // Pre-processing
+
+    // 비밀번호를 바꿀 때만 
+    // 비밀번호를 암호화한다. 
+    let user = this;
+    if (user.isModified("password")) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) {
+                return next(err);
+            } else {
+                bcrypt.hash(user.password, salt, function(err, hash) {
+                    if (err) {
+                        return next(err);
+                    } else {
+                        user.password = hash;
+                        next();
+                    }
+                });
+            }
+        });
     }
 });
 
